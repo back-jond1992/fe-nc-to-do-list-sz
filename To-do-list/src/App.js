d31f7067f0f6b4319478b1ad0a1689671039366b
Kyle Povey <povey.kyle@gmail.com>
2021-11-11 12:10:18 +0000

    added stufftodo component

---
+++ To-do-list/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/header";
+import StuffToDo from "./components/stuffToDo";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+      <StuffToDo />
+    </div>
+  );
+}
+
+export default App;
---
+++ To-do-list/src/components/stuffToDo.js
@@ -0,0 +1,12 @@
+const StuffToDo = () => {
+  return (
+    <ul>
+      <li>Make dinner</li>
+      <li>Watch Netflix's</li>
+      <li>Cry myself sleep</li>
+      <li>Ran out of things to do</li>
+    </ul>
+  );
+};
+
+export default StuffToDo;
